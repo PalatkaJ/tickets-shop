52b1604353484f76feaaa5def4b237fe8ed74cbd
Jan Palatka <jankopalatka@seznam.cz>
2026-06-25 14:29:17 +0200

    adjust run.sh to mirror the directory

--- run.sh
+++ run.sh
@@ -1,1 +1,6 @@
-docker build -t simple-tickets-app . && docker run --rm -it simple-tickets-app
+docker build -t simple-tickets-app . && \
+docker run --rm -it \
+  -v "$(pwd)":/tickets-shop \
+  -v /tickets-shop/bin \
+  -v /tickets-shop/obj \
+  simple-tickets-app
